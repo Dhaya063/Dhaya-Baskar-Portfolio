98b9b55a2a01faa908c21219cfbac6ea1ffa2a5c
Dhaya063 <dhayab2021@gmail.com>
2024-06-19 15:40:22 +0530

    made responsive changes

--- style.js
+++ style.js
@@ -1,24 +1,20 @@
-
 // Toggle Nav bar
-
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 let sections = document.querySelectorAll('section');
 
 menuIcon.onclick = () => {
-      menuIcon.classList.toggle('fa-x');
-    navbar.classList.toggle('active');
+    menuIcon.classList.toggle('fa-x');
+    navbar.classList.toggle('active');
 
-    // Adjust section position when the navigation bar is active
-    if (navbar.classList.contains('active')) {
-        sections.forEach((section) => {
-            section.style.transform = 'translateY(' + navbar.offsetHeight + 'px)';
-        });
-    } else {
-        sections.forEach((section) => {
-            section.style.transform = 'translateY(0)';
-        });
-    }
+    // Adjust section position when the navigation bar is active
+    if (navbar.classList.contains('active')) {
+        sections.forEach((section) => {
+            section.style.transform = 'translateY(' + navbar.offsetHeight + 'px)';
+        });
+    } else {
+        sections.forEach((section) => {
+            section.style.transform = 'translateY(0)';
+        });
+    }
 };
-
-
